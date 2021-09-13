bb947fbc8f0a4f19c22f3a2c46da340d92c5de71
srikrishna777 <70716833+srikrishna777@users.noreply.github.com>
2021-09-13 08:32:04 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,10 @@
+name = 'mary'
+password = 'pokemon'
+if name == 'Mary':
+    print('Hello ,Mary')
+    if password == 'pokemon':
+        print('Access granted')
+    else:
+        print('Wrong password')
+
+
